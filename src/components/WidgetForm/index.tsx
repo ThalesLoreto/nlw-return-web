88b7bada44f59899080458e2486db2097374fea8
ThalesLoreto <tloreto.dev@gmail.com>
2022-05-03 17:10:29 -0300

    finish content header

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-import { CloseButton } from '../CloseButton'
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
 
 import bugImageUrl from '../../../assets/bug.svg'
 import ideaImageUrl from '../../../assets/idea.svg'
@@ -36,13 +36,20 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+  function handleRestartFeedback() {
+    setFeedbackType(null)
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
       {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello, world!</p>
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
 
       <footer>
